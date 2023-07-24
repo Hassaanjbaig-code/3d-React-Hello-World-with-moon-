3ee4a56ed565ab45aea6bd39a4dd9908d5166973
Hassaanjbaig-code <hassaanb000@gmail.com>
2023-07-24 03:43:28 -0700

    Update the text

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import { Canvas } from '@react-three/fiber';
 import Moon from './Components/moon';
 import { Center, OrbitControls, Stars, Text3D } from '@react-three/drei';
-// import Text3D from './Components/Text';
 import { Perf } from 'r3f-perf';
-import font from './Font/Roboto Medium_Regular.json';
+import font from './Font/Roboto Black_Regular.json';
+import Mfont from './Font/Roboto Medium_Regular.json';
 
 function App() {
   return (
@@ -16,13 +16,19 @@ function App() {
         <ambientLight intensity={0.9} />
         <spotLight position={[20, 40, 10]} />
         <color attach='background' args={['#121212']} />
-        {/* <mesh position={[-8, -2, 0]}> */}
         <Center>
-          <Text3D font={font} size={2} height={0.4} >
-            Hassaan Baig
+          <Text3D
+            material-color = {'cyan'}
+            font={font}
+            size={2}
+            height={0.2}
+          >
+            Hassan Baig
           </Text3D>
         </Center>
-        {/* </mesh> */}
+        <mesh position={[-5, -2, 0]}>
+          <Text3D font={Mfont} size={0.3} height={0} material-color = {'cyan'} >It is nighttime, but do not forget that the day is coming.</Text3D>
+        </mesh>
         <Moon />
       </Canvas>
     </div>
